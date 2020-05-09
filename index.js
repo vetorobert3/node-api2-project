d1c2c2165221a4cefab8a362cc8e76364ea9e1e3
Veto Ramirez <vetorobert3@gmail.com>
2020-05-09 09:48:32 -0700

    starting on project. Made separate server.js file for routing purposes.

---
+++ index.js
@@ -0,0 +1,10 @@
+// add an endpoint that returns all the messages for a hub
+// add an endpoint for adding new message to a hub
+
+const app = require('./server.js');
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
+});
