dcd907017d6ab9392b9536029465584a258cfb05
Veto Ramirez <vetorobert3@gmail.com>
2020-05-12 20:44:56 -0700

    Getting code from other project.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// add an endpoint that returns all the messages for a hub
-// add an endpoint for adding new message to a hub
 const app = require('./server.js');
 
 const PORT = 4000;
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const server = express();
-const db = require('./data/db.js');
+const hubsRouter = require('./data/post-router.js');
 
 server.use(express.json());
-server.use('/api/data', db);
+server.use('/api/posts', hubsRouter);
 
 server.get('/', (req, res) => {
   res.send(`
-    <h2>Blog Posts</h2>
+    <h2>Lambda Hubs API</h>
+    <p>Welcome to the Lambda Hubs API</p>
   `);
 });
 
-module.export = server;
+module.exports = server;
